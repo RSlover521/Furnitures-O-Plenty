5afc379dc686e80627c61be698ecadde88657138
RSlover521 <wulance58@gmail.com>
2025-07-24 12:14:33 -0500

    added dependencies (project setup)

--- src/main/java/com/rslover521/furnituressoplenty/FurnituresOPlenty.java
+++ src/main/java/com/rslover521/furnituressoplenty/FurnituresOPlenty.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.rslover521.furnituressoplenty;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
@@ -6,15 +6,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
-{
+@Mod(FurnituresOPlenty.MODID)
+public class FurnituresOPlenty {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "furnituresoplenty";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod(FMLJavaModLoadingContext context) {
+    public FurnituresOPlenty(FMLJavaModLoadingContext context) {
     	LOGGER.info("hi");
     }
 }
