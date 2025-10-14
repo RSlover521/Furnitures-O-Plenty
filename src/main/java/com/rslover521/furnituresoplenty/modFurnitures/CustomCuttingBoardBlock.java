ec59964e898319c0497e95df9eda04a415c789f6
RSlover52111 <xwu053447@hsstu.lpsb.org>
2025-10-14 17:35:41 +0000

    Added register for all cutting board

---
+++ src/main/java/com/rslover521/furnituresoplenty/modFurnitures/CustomCuttingBoardBlock.java
@@ -0,0 +1,25 @@
+package com.rslover521.furnituresoplenty.modFurnitures;
+
+import com.mrcrayfish.furniture.refurbished.block.CuttingBoardBlock;
+
+import net.minecraft.world.level.block.state.properties.WoodType;
+import net.minecraft.world.level.material.MapColor;
+
+public class CustomCuttingBoardBlock extends CuttingBoardBlock {
+	private WoodType woodType;
+	public CustomCuttingBoardBlock(WoodType woodType) {
+		super(woodType, Properties.of()
+				.mapColor(MapColor.WOOD)
+				.strength(2.0F, 3.0F)
+		);
+		this.woodType = woodType;
+	}
+
+	public WoodType getWoodType() {
+		return woodType;
+	}
+
+	public void setWoodType(WoodType woodType) {
+		this.woodType = woodType;
+	}
+}
