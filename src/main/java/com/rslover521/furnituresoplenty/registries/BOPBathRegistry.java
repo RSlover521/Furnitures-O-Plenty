2e79601707bcc729049945eced1acd530022fbb8
RSlover52111 <xwu053447@hsstu.lpsb.org>
2025-10-16 12:15:05 +0000

    registered all bath blocks

--- src/main/java/com/rslover521/furnituresoplenty/registries/BOPBathRegistry.java
+++ src/main/java/com/rslover521/furnituresoplenty/registries/BOPBathRegistry.java
@@ -20,10 +20,34 @@ public class BOPBathRegistry {
 			DeferredRegister.create(ForgeRegistries.ITEMS, FurnituresOPlenty.MODID);
 
 	// === Biomes O' Plenty wood baths
-	public static RegistryObject<Block> FIR_BATH;
+	public static final RegistryObject<Block> FIR_BATH;
+	public static final RegistryObject<Block> PINE_BATH;
+	public static final RegistryObject<Block> MAPLE_BATH;
+	public static final RegistryObject<Block> REDWOOD_BATH;
+	public static final RegistryObject<Block> MAHOGANY_BATH;
+	public static final RegistryObject<Block> JACARANDA_BATH;
+	public static final RegistryObject<Block> PALM_BATH;
+	public static final RegistryObject<Block> WILLOW_BATH;
+	public static final RegistryObject<Block> DEAD_BATH;
+	public static final RegistryObject<Block> MAGIC_BATH;
+	public static final RegistryObject<Block> UMBRAN_BATH;
+	public static final RegistryObject<Block> HELLBARK_BATH;
+	public static final RegistryObject<Block> EMPYREAL_BATH;
 
 	static {
 		FIR_BATH = registerBath("fir", BOPWoodTypes.FIR);
+		PINE_BATH = registerBath("pine", BOPWoodTypes.PINE);
+		MAPLE_BATH = registerBath("maple", BOPWoodTypes.MAPLE);
+		REDWOOD_BATH = registerBath("redwood", BOPWoodTypes.REDWOOD);
+		MAHOGANY_BATH = registerBath("mahogany", BOPWoodTypes.MAHOGANY);
+		JACARANDA_BATH = registerBath("jacaranda", BOPWoodTypes.JACARANDA);
+		PALM_BATH = registerBath("palm", BOPWoodTypes.PALM);
+		WILLOW_BATH = registerBath("willow", BOPWoodTypes.WILLOW);
+		DEAD_BATH = registerBath("dead", BOPWoodTypes.DEAD);
+		MAGIC_BATH = registerBath("magic", BOPWoodTypes.MAGIC);
+		UMBRAN_BATH = registerBath("umbran", BOPWoodTypes.UMBRAN);
+		HELLBARK_BATH = registerBath("hellbark", BOPWoodTypes.HELLBARK);
+		EMPYREAL_BATH = registerBath("empyreal", BOPWoodTypes.EMPYREAL);
 	}
 
 	private static RegistryObject<Block> registerBath(String name, WoodType woodType) {
