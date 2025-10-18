34c47e58cdb5943d9bee169b17b316b99d325667
RSlover521 <wulance58@gmail.com>
2025-10-17 21:08:22 -0500

    finished Mod creative mode tabs

--- src/main/java/com/rslover521/furnituresoplenty/registries/BOPStorageCabinetRegistry.java
+++ src/main/java/com/rslover521/furnituresoplenty/registries/BOPStorageCabinetRegistry.java
@@ -20,34 +20,34 @@ public class BOPStorageCabinetRegistry {
 			DeferredRegister.create(ForgeRegistries.ITEMS, FurnituresOPlenty.MODID);
 			
 	// === Biomes O' Plenty wood storage cabinets
-	public static final RegistryObject<Block> FIR_STORAGE_CABINETS;
-	public static final RegistryObject<Block> PINE_STORAGE_CABINETS;
-	public static final RegistryObject<Block> MAPLE_STORAGE_CABINETS;
-	public static final RegistryObject<Block> REDWOOD_STORAGE_CABINETS;
-	public static final RegistryObject<Block> MAHOGANY_STORAGE_CABINETS;
-	public static final RegistryObject<Block> JACARANDA_STORAGE_CABINETS;
-	public static final RegistryObject<Block> PALM_STORAGE_CABINETS;
-	public static final RegistryObject<Block> WILLOW_STORAGE_CABINETS;
-	public static final RegistryObject<Block> DEAD_STORAGE_CABINETS;
-	public static final RegistryObject<Block> MAGIC_STORAGE_CABINETS;
-	public static final RegistryObject<Block> UMBRAN_STORAGE_CABINETS;
-	public static final RegistryObject<Block> HELLBARK_STORAGE_CABINETS;
-	public static final RegistryObject<Block> EMPYREAL_STORAGE_CABINETS;
+	public static final RegistryObject<Block> FIR_STORAGE_CABINET;
+	public static final RegistryObject<Block> PINE_STORAGE_CABINET;
+	public static final RegistryObject<Block> MAPLE_STORAGE_CABINET;
+	public static final RegistryObject<Block> REDWOOD_STORAGE_CABINET;
+	public static final RegistryObject<Block> MAHOGANY_STORAGE_CABINET;
+	public static final RegistryObject<Block> JACARANDA_STORAGE_CABINET;
+	public static final RegistryObject<Block> PALM_STORAGE_CABINET;
+	public static final RegistryObject<Block> WILLOW_STORAGE_CABINET;
+	public static final RegistryObject<Block> DEAD_STORAGE_CABINET;
+	public static final RegistryObject<Block> MAGIC_STORAGE_CABINET;
+	public static final RegistryObject<Block> UMBRAN_STORAGE_CABINET;
+	public static final RegistryObject<Block> HELLBARK_STORAGE_CABINET;
+	public static final RegistryObject<Block> EMPYREAL_STORAGE_CABINET;
 
 	static {
-		FIR_STORAGE_CABINETS = registerStorageCabinets("fir", BOPWoodTypes.FIR);
-		PINE_STORAGE_CABINETS = registerStorageCabinets("pine", BOPWoodTypes.PINE);
-		MAPLE_STORAGE_CABINETS = registerStorageCabinets("maple", BOPWoodTypes.MAPLE);
-		REDWOOD_STORAGE_CABINETS = registerStorageCabinets("redwood", BOPWoodTypes.REDWOOD);
-		MAHOGANY_STORAGE_CABINETS = registerStorageCabinets("mahogany", BOPWoodTypes.MAHOGANY);
-		JACARANDA_STORAGE_CABINETS = registerStorageCabinets("jacaranda", BOPWoodTypes.JACARANDA);
-		PALM_STORAGE_CABINETS = registerStorageCabinets("palm", BOPWoodTypes.PALM);
-		WILLOW_STORAGE_CABINETS = registerStorageCabinets("willow", BOPWoodTypes.WILLOW);
-		DEAD_STORAGE_CABINETS = registerStorageCabinets("dead", BOPWoodTypes.DEAD);
-		MAGIC_STORAGE_CABINETS = registerStorageCabinets("magic", BOPWoodTypes.MAGIC);
-		UMBRAN_STORAGE_CABINETS = registerStorageCabinets("umbran", BOPWoodTypes.UMBRAN);
-		HELLBARK_STORAGE_CABINETS = registerStorageCabinets("hellbark", BOPWoodTypes.HELLBARK);
-		EMPYREAL_STORAGE_CABINETS = registerStorageCabinets("empyreal", BOPWoodTypes.EMPYREAL);
+		FIR_STORAGE_CABINET = registerStorageCabinets("fir", BOPWoodTypes.FIR);
+		PINE_STORAGE_CABINET = registerStorageCabinets("pine", BOPWoodTypes.PINE);
+		MAPLE_STORAGE_CABINET = registerStorageCabinets("maple", BOPWoodTypes.MAPLE);
+		REDWOOD_STORAGE_CABINET = registerStorageCabinets("redwood", BOPWoodTypes.REDWOOD);
+		MAHOGANY_STORAGE_CABINET = registerStorageCabinets("mahogany", BOPWoodTypes.MAHOGANY);
+		JACARANDA_STORAGE_CABINET = registerStorageCabinets("jacaranda", BOPWoodTypes.JACARANDA);
+		PALM_STORAGE_CABINET = registerStorageCabinets("palm", BOPWoodTypes.PALM);
+		WILLOW_STORAGE_CABINET = registerStorageCabinets("willow", BOPWoodTypes.WILLOW);
+		DEAD_STORAGE_CABINET = registerStorageCabinets("dead", BOPWoodTypes.DEAD);
+		MAGIC_STORAGE_CABINET = registerStorageCabinets("magic", BOPWoodTypes.MAGIC);
+		UMBRAN_STORAGE_CABINET = registerStorageCabinets("umbran", BOPWoodTypes.UMBRAN);
+		HELLBARK_STORAGE_CABINET = registerStorageCabinets("hellbark", BOPWoodTypes.HELLBARK);
+		EMPYREAL_STORAGE_CABINET = registerStorageCabinets("empyreal", BOPWoodTypes.EMPYREAL);
 	}
 
 	private static RegistryObject<Block> registerStorageCabinets(String name, WoodType woodType) {
