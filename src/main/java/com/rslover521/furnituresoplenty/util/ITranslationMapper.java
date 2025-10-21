3cc14b027fd3b23aeb0cac89848df2752ce7c232
RSlover52111 <xwu053447@hsstu.lpsb.org>
2025-10-21 15:35:14 +0000

    added ITranslationMapper interface for easier use with translations other than english

--- src/main/java/com/rslover521/furnituresoplenty/util/ITranslationMapper.java
+++ src/main/java/com/rslover521/furnituresoplenty/util/ITranslationMapper.java
@@ -1,6 +1,6 @@
 package com.rslover521.furnituresoplenty.util;
 
 @FunctionalInterface
-public interface TranslationMapper {
+public interface ITranslationMapper {
 	String map(String wood, String type);
 }
