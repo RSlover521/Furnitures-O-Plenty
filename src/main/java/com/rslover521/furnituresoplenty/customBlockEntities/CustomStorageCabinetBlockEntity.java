2d37be07ff4d88ad0320592ed9f061f928495dd7
RSlover5211 <wxinchen12345@outlook.com>
2025-10-31 14:52:39 +0000

    fixed codespaces weird spacing

---
+++ src/main/java/com/rslover521/furnituresoplenty/customBlockEntities/CustomStorageCabinetBlockEntity.java
@@ -0,0 +1,7 @@
+package com.rslover521.furnituresoplenty.customBlockEntities;
+
+public class CustomStorageCabinetBlockEntity extends StorageCabinetBlockEntity {
+    public CustomStorageCabinetBlockEntity(BlockPos pos, BlockState state) {
+    super(pos, state);
+    }
+}
