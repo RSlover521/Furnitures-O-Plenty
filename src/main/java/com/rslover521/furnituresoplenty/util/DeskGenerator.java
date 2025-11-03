8100d4d5b254bcb8275e8c730a2a490291ec569f
RSlover52111 <xwu053447@hsstu.lpsb.org>
2025-11-03 21:32:17 +0000

    added json files for desk and cutting board

--- src/main/java/com/rslover521/furnituresoplenty/util/DeskGenerator.java
+++ src/main/java/com/rslover521/furnituresoplenty/util/DeskGenerator.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class DeskModelGenerator {
+public class DeskGenerator {
 
     private static final String MOD_ID = "furnituresoplenty";
 
--- src/main/java/com/rslover521/furnituresoplenty/util/TableGenerator.java
+++ src/main/java/com/rslover521/furnituresoplenty/util/TableGenerator.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FurnitureOPlentyTableGenerator {
+public class TableGenerator {
 
     private static final String MODID = "furnituresoplenty";
 
