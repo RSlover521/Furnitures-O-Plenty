2dd011506b6d32cb1274e1d7b8328fb4fe866f80
RSlover52111 <xwu053447@hsstu.lpsb.org>
2025-11-21 14:13:40 +0000

    made run.sh executable directly on terminal

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 set -e
 
 echo "Compiling ModValidator..."
-mkdir -p out
-javac -cp "$CLASSPATH" -d out src/main/java/com/rslover521/furnituresoplenty/util/validators/ModValidator.java
-
 echo "Running ModValidator..."
-java -cp out:$CLASSPATH com.rslover521.furnituresoplenty.util.validators.ModValidator
+cd /workspaces/Furnitures-O-Plenty ; /usr/bin/env /usr/local/sdkman/candidates/java/17.0.15-ms/bin/java @/tmp/cp_6pdy4t72o292ss76ms9thnapm.argfile com.rslover521.furnituresoplenty.util.validators.ModValidator
 echo "Mod validation completed."
